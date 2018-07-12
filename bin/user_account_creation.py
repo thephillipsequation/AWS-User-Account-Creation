6c0c320f09c1114a5c184815135a381c57ff5fd4
Josh Phillips <joshua.phillips@stelligent.com>
2018-07-12 13:29:23 -0400

    minor refactor

--- bin/user_account_creation.py
+++ bin/user_account_creation.py
@@ -10,10 +10,7 @@ def create_user(client, username, path="\"):
 def main():
     test_user = "test1"
     iam = boto3.client('iam')
-    iam.create_user(
-        UserName=test_user
-
-    )
+    create_user(iam, test_user)
     print(iam.get_user(
         UserName=test_user
     ))
