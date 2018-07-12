a732cf8b4cb9c0bbe8133726b632a2c9f17e710e
Josh Phillips <joshua.phillips@stelligent.com>
2018-07-12 14:15:59 -0400

    adding error handling

--- bin/user_account_creation.py
+++ bin/user_account_creation.py
@@ -6,14 +6,29 @@ def create_user(client, username):
         UserName=username
         )
 
+def delete_user(client, username):
+    client.delete_user(
+        UserName=username
+    )
+
+def user_exists(client, username):
+    try:
+        resp = client.get_user(
+        UserName=username
+        )
+        print("User {} successfully created".format(username))
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'NoSuchEntity':
+            print("User {} Already Exists".format(username))
+
 def main():
     test_user = "test1"
     iam = boto3.client('iam')
+    delete_user(iam, test_user)
     create_user(iam, test_user)
-    print(iam.get_user(
-        UserName=test_user
-    ))
-    print({}).format(test_user)
+    user_exists(iam, test_user)
+    user_exists(iam, "steve")
+    
 
 if __name__== "__main__":
   main()
