be104ea9b3b295f74c893f353fecbfff97ed4918
Josh Phillips <joshua.phillips@stelligent.com>
2018-07-11 22:45:19 -0400

    adding basic test case

--- bin/user_account_creation.py
+++ bin/user_account_creation.py
@@ -5,6 +5,16 @@ from botocore.exceptions import ClientError, ParamValidationError
 
 
 def main():
+    test_user = "test1"
+    iam = boto3.client('iam')
+    iam.create_user(
+        path="/test/",
+        UserName=test_user
+
+    )
+    iam.get_user(
+        UserName=test_user
+    )
     print("Hello World")
 
 if __name__== "__main__":
