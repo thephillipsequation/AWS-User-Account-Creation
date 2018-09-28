793bae48e36ad69f94b1ca342881a219e4f4091b
Josh Phillips <joshua.phillips@stelligent.com>
2018-09-28 15:45:50 -0400

    fixing our delete user tests

--- tests/test_user_account_creation.py
+++ tests/test_user_account_creation.py
@@ -4,8 +4,6 @@ import user_account_creation.main as uac
 
 
 
-
-
 @mock_iam
 def test_user_creation():
     client = boto3.client('iam')
@@ -14,6 +12,10 @@ def test_user_creation():
     assert uac.user_exists(client, username) is True
 
 @mock_iam
-
-
-
+def test_delete_user():
+    client = boto3.client('iam')
+    username = 'thephillipsequation'
+    uac.create_user(client, username)
+    uac.delete_user(client, username)
+    assert uac.user_exists(client, username) is False
+    
