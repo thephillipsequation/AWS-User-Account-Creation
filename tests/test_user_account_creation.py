603b55e1dd8b9073f2642003fa4b02b64c128f9a
Josh Phillips <joshua.phillips@stelligent.com>
2018-10-26 14:18:37 -0400

    totally broken

--- tests/test_user_account_creation.py
+++ tests/test_user_account_creation.py
@@ -1,21 +1,29 @@
 import boto3
-from moto import mock_iam
 import user_account_creation.main as uac
+import os.path
+from moto import mock_iam
 
 
-
-@mock_iam
-def test_user_creation():
-    client = boto3.client('iam')
-    username = 'thephillipsequation'
-    uac.create_user(client, username)
-    assert uac.user_exists(client, username) is True
+# def test_password_generation():
+#     assert 
 
 @mock_iam
 def test_delete_user():
     client = boto3.client('iam')
     username = 'thephillipsequation'
-    uac.create_user(client, username)
+    uac.create_user(client, username, password="test1234")
     uac.delete_user(client, username)
     assert uac.user_exists(client, username) is False
+
+@mock_iam
+def test_user_creation():
+    client = boto3.client('iam')
+    username = 'thephillipsequation'
+    uac.create_user(client, username, password="test1234")
+    assert uac.user_exists(client, username) is True
+
+
+
+
+
     
--- user_account_creation/main.py
+++ user_account_creation/main.py
@@ -2,28 +2,38 @@
 This module creates IAM users given a yaml file saying listing their name and group
 '''
 import boto3
+import string
 from botocore.exceptions import ClientError
+from pathlib import Path
+from secrets import choice
 
-def create_user(client, username):
-    '''Create an IAM user'''
-    response = client.create_user(
-        UserName=username
-        )
-    print(response)
 
-def update_login_profile(client, username, password, reset=True):
+def generate_password(dict_file='/usr/share/dict/words'):
+    try:
+        with open(dict_file) as f:
+            words = [word.strip() for word in f]
+            password = password = '_'.join(choice(words) for i in range(4))
+        return password
+    except IOError as error:
+        print('The dictionary file {} could not be found, please ensure this file is present on your system'.format(dict_file))
+        return error
+     
+
+def create_login_profile(client, username, password, reset=True):
     '''
         Provide console access for a user
+        resource: i.e: `iam = boto3.resource('iam)`
         username: must be a valid IAM username
         password: must comply with the IAM password policy
         reset: boolean value denoting if a password reset is required
     '''
-    response = client.update_login_profile(
-        UserName='string',
-        Password='string',
+    response = client.create_login_profile(
+        UserName=username,
+        Password=password,
         PasswordResetRequired=reset
     )
     print(response)
+    return 
 
 def delete_user(client, username):
     '''
@@ -42,21 +52,40 @@ def user_exists(client, username):
         resp = client.get_user(
             UserName=username
         )
-        print("User {} exists".format(username))
+        print("Username {} already exists. Please provide a unique username".format(username))
         return True
     except ClientError as error:
         if error.response['Error']['Code'] == 'NoSuchEntity':
             print("User {} does not exist".format(username))
             return False
+        else:
+            print(error)
 
-def main():
+
+def create_user(client, username, password):
+    '''Create an IAM user'''
+    if user_exists(client, username) == True:
+        return
+    elif user_exists(client, username) == False:
+        response = client.create_user(
+            UserName=username
+            )
+        create_login_profile(client, username, password)
+        print()
+        return
+        
+
+
+def main(test=True):
     '''Main function'''
-    test_user = "test1"
-    iam = boto3.client('iam')
-    create_user(iam, test_user)
-    user_exists(iam, test_user)
-    delete_user(iam, test_user)
-    user_exists(iam, "steve")
-
+    if test == True:
+        test_password = generate_password()
+        test_client =  boto3.client('iam')
+        test_username = 'test123'
+        if user_exists(test_client, test_username):
+            delete_user(test_client, test_username)
+        print(test_password)
+        print(test_client.get_account_password_policy())
+        create_user(test_client, test_username, test_password)
 if __name__ == "__main__":
     main()
